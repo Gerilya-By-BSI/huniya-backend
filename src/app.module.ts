672c87cb402d5973b7d14d6f441dc80562a481a2
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2025-04-30 16:24:40 +0700

    Refactor AppController and AppModule to remove AppService and standardize API response structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,36 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module, ValidationPipe } from '@nestjs/common';
+import { AppController } from '@/app.controller';
+import { AuthModule } from '@/auth/auth.module';
+import { PrismaModule } from '@/prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+import { PrismaExceptionFilter } from '@/common/exceptions';
+import { TransformInterceptor } from '@/common/interceptors/transform.interceptor';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    AuthModule,
+  ],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+      }),
+    },
+    {
+      provide: APP_FILTER,
+      useClass: PrismaExceptionFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TransformInterceptor,
+    },
+  ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
