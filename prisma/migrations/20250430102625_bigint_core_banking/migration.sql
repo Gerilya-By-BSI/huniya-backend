36eac4be971b2aa09c448308c25df149192d804f
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2025-04-30 17:29:12 +0700

    Migrate CoreBankingUser fields to BigInt for improved financial data handling

---
+++ prisma/migrations/20250430102625_bigint_core_banking/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "core_banking_users" ALTER COLUMN "annual_income" SET DATA TYPE BIGINT,
+ALTER COLUMN "monthly_inhand_salary" SET DATA TYPE BIGINT,
+ALTER COLUMN "total_emi_per_month" SET DATA TYPE BIGINT,
+ALTER COLUMN "monthly_balance" SET DATA TYPE BIGINT;
