cff87ce4facc39be13eb103f7d5c6586f07449ad
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2025-04-30 16:55:28 +0700

    Add protected and admin routes with appropriate guards in AppController

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AdminGuard, JwtGuard } from './auth/guard';
 
 @Controller()
 export class AppController {
@@ -6,6 +7,24 @@ export class AppController {
   index() {
     return {
       message: 'Huniya API',
+      version: '1.0.0',
+    };
+  }
+
+  @UseGuards(JwtGuard)
+  @Get('/protected')
+  protected() {
+    return {
+      message: 'Huniya API Protected',
+      version: '1.0.0',
+    };
+  }
+
+  @UseGuards(JwtGuard, AdminGuard)
+  @Get('/admin')
+  admin() {
+    return {
+      message: 'Huniya API Admin Check',
       version: '1.0.0',
     };
   }
