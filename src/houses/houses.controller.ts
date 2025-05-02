884c9243845885911ca56492c6a03be255bae81b
farah-husna <farahhusnanabilah@gmail.com>
2025-05-02 07:50:45 +0700

    update response filter house

--- src/houses/houses.controller.ts
+++ src/houses/houses.controller.ts
@@ -3,15 +3,23 @@ import { HousesService } from './houses.service';
 import { JwtGuard } from '@/auth/guard';
 import { QueryHouseDto } from './dto/query-house.dto';
 import { BaseResponseDto } from '@/common/dto/base-response.dto';
+import { NotFoundException } from '@nestjs/common';
+
 
 @UseGuards(JwtGuard)
 @Controller('houses')
 export class HousesController {
   constructor(private readonly housesService: HousesService) {}
 
+
   @Get()
   async findAll(@Query() query: QueryHouseDto) {
     const { data, totalData } = await this.housesService.findAll(query);
+
+    if (!data || data.length === 0) {
+      throw new NotFoundException('No houses found');
+    }
+
     return new BaseResponseDto(true, 'Houses fetched successfully', {
       total_data: totalData,
       data,
