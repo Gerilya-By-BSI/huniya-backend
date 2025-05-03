c3755579c98ecf2a3e39cbe0d9264c3b63a14e6e
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2025-05-03 17:05:19 +0700

    Add HttpModule import to HousesModule for HTTP requests

--- src/houses/houses.module.ts
+++ src/houses/houses.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { HousesService } from './houses.service';
 import { HousesController } from './houses.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   controllers: [HousesController],
   providers: [HousesService],
 })
