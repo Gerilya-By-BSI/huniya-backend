0b52e0be241262230457b6ae29930a7ec387a6fa
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2025-05-03 14:32:36 +0700

    config docker

--- fastapi-ml/main.py
+++ fastapi-ml/main.py
@@ -23,7 +23,7 @@ app.add_middleware(
 )
 
 # Configure logging
-logging.basicConfig(
+logging.basicConfig(    
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
 )
