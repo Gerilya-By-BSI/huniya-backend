7412f02393b5ee0d66f0c6427d45bbc1849983ec
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2025-05-04 19:05:16 +0700

    Refactor AdminController and AdminService to use QueryBookmarkHouseDto for pagination and streamline data fetching

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { AdminController } from './admin.controller';
+import { PaginationModule } from '@/pagination/pagination.module';
 
 @Module({
+  imports: [PaginationModule],
   controllers: [AdminController],
   providers: [AdminService],
 })
