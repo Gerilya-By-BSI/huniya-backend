bf5e6b4bdaa62787626192c33193f3923d643c40
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2025-05-04 11:04:44 +0700

    Comment out DATABASE_URL in docker-compose.yml and log it in main.ts for debugging purposes

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,8 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') || 4444;
 
+  console.log('DATABASE_URL:', process.env.DATABASE_URL);
+
   await app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
   });
